e553bea725b533d1db4ffb4d5ee4325472c46496
Muhammad riski fauzi <muhammadriskifauzi10@gmail.com>
2022-03-01 23:20:58 +0700

    Selesai 1

--- opencv/read-image.py
+++ opencv/read-image.py
@@ -1,7 +1,12 @@
+import os
 import cv2 as cv
 
-img = cv.imread('assets/images/img1.jpg')
+os.system('cls')
 
-cv.imshow('Display', img)
+img = cv.imread('assets/images/img1.jpg', cv.COLOR_BGR2GRAY)
+
+img_r = cv.resize(img, (570, 720))
+
+cv.imshow('Display', des1)
 
 cv.waitKey(0)
