79db6ad6e338000d3d0974378a012f7103f4d57b
Muhammad riski fauzi <muhammadriskifauzi10@gmail.com>
2022-03-04 20:44:01 +0700

    Selesai belajar detect masker di file face-detect2.py pada F opencv part1

---
+++ opencv/face-detect2.py
@@ -0,0 +1,17 @@
+from operator import le
+import os
+import cv2 as cv
+
+os.system('cls')
+
+img = cv.imread('assets/images/img1.jpg')
+
+img_r = cv.resize(img, (500, 700))
+
+detect_face = cv.CascadeClassifier('assets/xml/nose.xml')
+
+detect_finish = detect_face.detectMultiScale(img_r, scaleFactor=1.2, minNeighbors=12)
+
+print(len(detect_finish))
+
+cv.waitKey(0)
